c1b24f2db5223b6388d3511ae763fe402c7e9d80
Tony <tony.kiplagat@student.moringaschool.com>
2023-06-14 20:48:34 +0300

    Done with arithmetic labs

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
-
 // Write your code here
+let num1 = 2;
+let num2 = 31;
+let multiply = num1 * num2;
+console.log(multiply);
+
+let random = Math.floor(Math.random() * 10) + 1;
+
+let num3 = 14;
+let num4 = 5;
+let mod = num3 % num4;
+console.log(mod);
+
+let max = Math.max(2, 4, 6, 16, 20);
+console.log(max);
